7c078ed4c301a698300dc29b48096c889d415608
Guilherme Storck <gstorck@fitec.org.br>
2015-11-27 23:23:32 -0500

    Initial commit. Created signal, a component and a layout

---
+++ src/main/java/com/storck/lazyandriodprogrammer/signals/OnSignal.java
@@ -0,0 +1,15 @@
+package com.storck.lazyandriodprogrammer.signals;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/*
+METHODS ANNOTATED WITH THIS MUST BE PUBLIC
+DO NOT USE PRIMITIVES AS PARAM TYPES ON METHODS ANNOTATED WITH THIS INTERFACE
+ */
+@Target(ElementType.METHOD)
+public @interface OnSignal {
+
+    String value();
+
+}
