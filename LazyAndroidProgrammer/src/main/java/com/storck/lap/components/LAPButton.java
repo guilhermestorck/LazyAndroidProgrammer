4cfe02d77ab136f6135dd593cbe21da697ba442c
Guilherme Storck <gstorck@fitec.org.br>
2015-11-29 16:40:22 -0500

    Initial commit

---
+++ LazyAndroidProgrammer/src/main/java/com/storck/lap/components/LAPButton.java
@@ -0,0 +1,17 @@
+package com.storck.lap.components;
+
+import android.content.Context;
+import android.widget.Button;
+
+public class LAPButton extends Button {
+
+    public LAPButton(Context context, String label) {
+        this(context, label, null);
+    }
+
+    public LAPButton(Context context, String label, OnClickListener callback) {
+        super(context);
+        setText(label);
+        setOnClickListener(callback);
+    }
+}
